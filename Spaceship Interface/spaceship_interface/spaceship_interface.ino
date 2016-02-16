2ac0ee4533d1db462becad0fff0782af40af8646
Brian Wilson <email@brian-wilson.org>
2016-02-16 22:12:56 +0000

    Adding Spaceship Interface

---
+++ Spaceship Interface/spaceship_interface/spaceship_interface.ino
@@ -0,0 +1,31 @@
+int switchstate = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchstate = digitalRead(2);
+  //this is a comment
+  if (switchstate == LOW) {
+    //the button is not pressed
+    digitalWrite(3, HIGH); //green LED
+    digitalWrite(4, LOW); //red LED
+    digitalWrite(5, LOW); //red LED
+  }
+  else { // the button is pressed
+    digitalWrite(3, LOW); //green LED
+    digitalWrite(4, LOW); //red LED
+    digitalWrite(5, HIGH); //red LED
+    delay(250); //wait for a quarter of a second
+    //toggle the LEDs
+    digitalWrite(4, HIGH); //red LED
+    digitalWrite(5, LOW); //red LED
+    delay(250); //wait for a further quarter of a second
+  }
+} //go back to the beginning of the loop
