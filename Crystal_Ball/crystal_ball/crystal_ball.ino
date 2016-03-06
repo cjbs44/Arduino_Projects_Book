11734032e47ac4ef27881484be62d4ce5bd3e126
Brian Wilson <email@brian-wilson.org>
2016-03-06 21:46:19 +0000

    adding Crystal Ball

---
+++ Crystal_Ball/crystal_ball/crystal_ball.ino
@@ -0,0 +1,62 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int switchpin = 6;
+int switchstate = 0;
+int prevswitchstate = 0;
+int reply;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  pinMode(switchpin, INPUT);
+  lcd.print("Ask the");
+  lcd.setCursor(0, 1);
+  lcd.print("Crystal Ball!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchstate = digitalRead(switchpin);
+  if (switchstate != prevswitchstate) {
+    if (switchstate == LOW) {
+      reply = random(8);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("The ball says:");
+      lcd.setCursor(0, 1);
+      switch(reply){
+        case 0:
+        lcd.print("Yes");
+        break;
+        case 1:
+        lcd.print("Most likely");
+        break;
+        case 2:
+        lcd.print("Certainly");
+        break;
+        case 3:
+        lcd.print("Outlook good");
+        break;
+        case 4:
+        lcd.print("Unsure");
+        break;
+        case 5:
+        lcd.print("Ask again");
+        break;
+        case 6:
+        lcd.print("Doubtful");
+        break;
+        case 7:
+        lcd.print("No");
+        break;
+      }
+      delay(5000);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Ask the");
+      lcd.setCursor(0, 1);
+      lcd.print("Crystal Ball!");
+    }
+  }
+  prevswitchstate = switchstate;
+}
